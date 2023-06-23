c70dfb8a1ce2043e8128f064ca14eaf6f4ea4d8b
Vicente González <vigb902@gmail.com>
2023-06-23 13:41:30 -0400

    t7 added

---
+++ Tareas/T7/compilar.c
@@ -0,0 +1,19 @@
+#define _POSIX_C_SOURCE 200809L
+#include <dirent.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+
+#include "pss.h"
+
+// ... agregue aca las funciones y variables globales adicionales que necesite
+
+int main(int argc, char *argv[]) {
+  // ... complete ...
+}
